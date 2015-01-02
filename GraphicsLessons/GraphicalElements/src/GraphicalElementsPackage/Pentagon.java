7cd439e2e993980fa0d2da0bc8fc6b094b55967f
Coder Dojo Swords <coderdojoswords1@gmail.com>
2015-01-02 10:41:03 +0000

    Added Pentagon
    aURGH

---
+++ GraphicsLessons/GraphicalElements/src/GraphicalElementsPackage/Pentagon.java
@@ -0,0 +1,16 @@
+package GraphicalElementsPackage;
+
+import java.awt.Color;
+
+public class Pentagon extends GraphicalPolygon {
+	  
+	public Pentagon(int x, int y, int pRadius, Color pColor)
+	{
+		super(x,y,pRadius, pColor);
+		numberOfSides = 5;
+	}
+	
+	
+	
+	
+}
